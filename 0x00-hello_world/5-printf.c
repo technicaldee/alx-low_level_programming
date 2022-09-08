e3b0703f164e938c8d857bf5f794449b655b3d88
Edidiong Udoh <gisthive@gmail.com>
2022-09-08 16:54:13 -0700

    initial

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammer, but the outcome is a piece of art,".
+ * Return: always 0.
+ */
+
+int main(void)
+{
+  printf("with proper grammer, but the outcome is a piece of art,\n");
+  return (0);
+}
