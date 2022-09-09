c64fb82be6661a1aec0e3a16b91e4ceb38c531d8
Edidiong Udoh <gisthive@gmail.com>
2022-09-09 15:26:17 -0700

    test

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - prints all possible combinations of single-digits
+ *separated by , and followe by a space
+ * Return: always 0.
+ */
+
+int main(void)
+{
 char alphabets;
 
-for (alphabets='a'; alphabets <= 'z'; alphabets++)
-  putchar(alphabets);
+for (alphabets='a'; alphabets <= 'z'; alphabets++){
+putchar(alphabets);
+}
 putchar('\n');
 return (0);
 }
