8a58d9c7da142a8d68038a8d475e4f1b6c80c190
Edidiong Udoh <gisthive@gmail.com>
2022-09-26 20:19:37 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* set_string - set value of a pointer to char
+* @s: pointer of a pointer
+* @to: pointee
+* Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Files for evem more pointers, arrays and strings project
