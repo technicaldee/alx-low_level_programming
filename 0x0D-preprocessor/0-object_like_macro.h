8d10a62a55dc4337bb4ba28795d4c22bb465f9e4
Edidiong Udoh <gisthive@gmail.com>
2022-10-10 19:50:14 -0700

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef foo_H
+#define foo_H
+
+
+#include <stdio.h>
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#ifndef foo_H
+#define foo_H
+
+#include <stdio.h>
+#define PI 3.14159265359
+
+#endif
