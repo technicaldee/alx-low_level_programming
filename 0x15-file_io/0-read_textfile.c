d91c21c81f67bc69a99bfbea6e63b84f4dd42b62
Edidiong Udoh <gisthive@gmail.com>
2022-10-31 14:54:14 -0700

    fixes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,12 +2,13 @@
 
 /**
  *read_textfile - reads a text file and prints the letters
-* @filename: filename.
-* @letters: numbers of letters printed.
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
-* Return: numbers of letters printed. It fails, returns 0.
-   */
-  ssize_t read_textfile(const char *filename, size_t letters)
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
 {
   int fd;
   ssize_t nrd, nwr;
